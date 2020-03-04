e880d41c083381b26ebeacdf518392499bd906d8
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2020-03-04 14:14:41 +0100

    fix test case

--- 1-isolate/challenge-3.js
+++ 1-isolate/challenge-3.js
@@ -4,7 +4,7 @@ const earlyReturn3Tests = [
   { name: 'Test 1', args: [4, '4'], expected: 'a is not a string' },
   { name: 'Test 2', args: ['4', 4], expected: 'b is not a string' },
   { name: 'Test 3', args: [4, 4], expected: 'a & b are not strings' },
-  { name: 'Test 4', args: [true, '3'], expected: ' a is not a string' },
+  { name: 'Test 4', args: [true, '3'], expected: 'a is not a string' },
   { name: 'Test 5', args: ['hi', '!'], expected: 'hi!' },
   { name: 'Test 6', args: ['by', 'e!'], expected: 'bye!' },
 ];
